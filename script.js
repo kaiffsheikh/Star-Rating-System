224c6ea6f15a335416879ddedf122050bbc8add5
Mohammad Kaif Sheikh <kaifsheikhf015@gmail.com>
2024-01-30 12:48:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("button");
+      const post = document.querySelector(".post");
+      const widget = document.querySelector(".star-widget");
+      const editBtn = document.querySelector(".edit");
+      btn.onclick = ()=>{
+        widget.style.display = "none";
+        post.style.display = "block";
+      }
